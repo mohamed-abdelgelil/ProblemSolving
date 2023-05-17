c43c7060d99eff8bb3a2b21b9f1a568146419940
Mohamed Abdo <m.abdelgelil.abdo@gmail.com>
2023-05-17 21:04:49 +0300

    longestCommonPrefix:14

--- src/main/java/Leetcode/longestCommonPrefix14.java
+++ src/main/java/Leetcode/longestCommonPrefix14.java
@@ -4,8 +4,7 @@ public class longestCommonPrefix14 {
     public static void main(String[] args) {
 
         System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
-        System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
-        System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
+       // System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
     }
 
     public static String longestCommonPrefix(String[] strs) {
@@ -13,9 +12,13 @@ public class longestCommonPrefix14 {
             return "";
         }
         String subS = "";
+        int i;
         for(int x = strs[0].length(); x >= 0; x--){
             subS = strs[0].substring(0,x);
-            int i;
+            if(subS.isEmpty())
+            {
+                return "";
+            }
             for(i=1;i < strs.length ; i++){
                 if(!strs[i].startsWith(subS)){
                     break;
