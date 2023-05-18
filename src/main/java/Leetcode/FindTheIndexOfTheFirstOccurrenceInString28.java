0101b8865b5c336ac9834bb9d239b66189f9a6a1
Mohamed Abdo <m.abdelgelil.abdo@gmail.com>
2023-05-18 11:48:27 +0300

    FindTheIndexOfTheFirstOccurrenceInString28

---
+++ src/main/java/Leetcode/FindTheIndexOfTheFirstOccurrenceInString28.java
@@ -0,0 +1,12 @@
+package Leetcode;
+
+public class FindTheIndexOfTheFirstOccurrenceInString28 {
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad","sad"));
+        System.out.println(strStr("leetcode","leeto"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
