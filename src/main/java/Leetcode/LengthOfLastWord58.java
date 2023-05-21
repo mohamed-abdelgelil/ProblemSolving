22c45efc80de7cf3a411aec6cbb4315c389d500b
Mohamed Abdo <m.abdelgelil.abdo@gmail.com>
2023-05-21 23:40:50 +0300

    LengthOfLastWord58

---
+++ src/main/java/Leetcode/LengthOfLastWord58.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class LengthOfLastWord58 {
+    public static int lengthOfLastWord(String s) {
+        int charCount = 0;
+        for(int i = s.length()-1; i>=0;i--){
+            if(s.charAt(i) != ' '){
+                charCount++;
+            }else if(charCount > 0){
+                return charCount;
+            }
+        }
+        return charCount;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("a"));
+        System.out.println(lengthOfLastWord("Hello World"));
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));
+    }
+
+}
